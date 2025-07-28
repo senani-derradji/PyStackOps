2672e0747d41cf857a3e1bf264e36f5d6e4239be
senani-derradji <derradji.senani.1@gmail.com>
2025-07-28 16:27:42 +0100

    v1.0.0

---
+++ app/app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, render_template, redirect
+import pymysql, os
+app = Flask(__name__)
+try:
+    db = pymysql.connect(
+        host=os.getenv("DB_HOST", "localhost"),
+        user=os.getenv("DB_USER", "derradji"),
+        password=os.getenv("DB_PASSWORD", "PASS1212"),
+        database=os.getenv("DB_NAME", "crudsdb"),
+        connect_timeout=5 )
+except pymysql.MySQLError as e: print(e)
+with db.cursor() as cursor:
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS messages (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            ip_address VARCHAR(100),
+            message TEXT
+        )
+    """)
+    db.commit()
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        msg = request.form['message']
+        ip = request.remote_addr
+        with db.cursor() as cursor:
+            cursor.execute("INSERT INTO messages (ip_address, message) VALUES (%s, %s)", (ip, msg))
+            db.commit()
+        return f"<h2>Your message: {msg}</h2><br><a href='/'>Back</a>"
+
+    return render_template('form.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
