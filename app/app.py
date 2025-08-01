23c19616dc2ce99e115017cd911f520681052040
senani-derradji <derradji.senani.1@gmail.com>
2025-08-01 22:07:31 +0100

    add redis to docker-compose + nginx caching

--- app/app.py
+++ app/app.py
@@ -5,6 +5,7 @@ from flask import Flask, request, render_template, redirect
 import pymysql, os
 
 app = Flask(__name__)
+
 try:
     db = pymysql.connect(
         host=os.getenv("DB_HOST", "localhost"),
@@ -12,7 +13,17 @@ try:
         password=os.getenv("DB_PASSWORD", "password"),
         database=os.getenv("DB_NAME", "name"),
         connect_timeout=5 )
-except pymysql.MySQLError as e: print(e)
+    print("Connected to the database successfully.")
+
+except pymysql.MySQLError as e:
+    print(f"Error connecting to the database: {e}")
+    db = None
+
+if db is None:
+    print("Database connection failed. Exiting the application.")
+    exit(1)
+
+# Create the messages table if it doesn't exist
 with db.cursor() as cursor:
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS messages (
@@ -22,17 +33,21 @@ with db.cursor() as cursor:
         )
     """)
     db.commit()
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         msg = request.form['message']
         ip = request.remote_addr
+
         with db.cursor() as cursor:
             cursor.execute("INSERT INTO messages (ip_address, message) VALUES (%s, %s)", (ip, msg))
             db.commit()
         return f"<h2>Your message: {msg}</h2><br><a href='/'>Back</a>"
 
     return render_template('form.html')
+
 
 @app.route("/app", methods=["GET"])
 def app_page():
