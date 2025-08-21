d7bccc0ede76f9e027732df47a2ef73b3e5216b2
Derradji Senani <89805807+senani-derradji@users.noreply.github.com>
2025-08-21 20:03:45 +0100

    Update apply.sh

--- k8s/K3s/DEV-TEST/apply.sh
+++ k8s/K3s/DEV-TEST/apply.sh
@@ -1,23 +1,23 @@
-kubectl apply -f ../Secrets/
-kubectl apply -f ../ConfigMaps/
-kubectl apply -f PVCs/
+kubectl apply -f ../../Secrets/
+kubectl apply -f ../../ConfigMaps/
+kubectl apply -f ../PVCs/
 
 echo "MYSQL"
-kubectl apply -f ../Deployments/Mysql-Deployment.yaml
-kubectl apply -f ../Services/Mysql-Service.yaml
+kubectl apply -f ../../Deployments/Mysql-Deployment.yaml
+kubectl apply -f ../../Services/Mysql-Service.yaml
 
 sleep 20
 echo "REDIS"
-kubectl apply -f ../Deployments/Redis-Deployment.yaml
-kubectl apply -f ../Services/Redis-Service.yaml
+kubectl apply -f ../../Deployments/Redis-Deployment.yaml
+kubectl apply -f ../../Services/Redis-Service.yaml
 sleep 5
 echo "Flask"
-kubectl apply -f ..//Deployments/Flask-Deployment.yaml
-kubectl apply -f ../Services/Flask-Service.yaml
+kubectl apply -f ../..//Deployments/Flask-Deployment.yaml
+kubectl apply -f ../../Services/Flask-Service.yaml
 sleep 5
 echo "Nginx"
-kubectl apply -f ../Deployments/Nginx-Deployment.yaml
-kubectl apply -f ../Services/Nginx-Service.yaml
+kubectl apply -f ../../Deployments/Nginx-Deployment.yaml
+kubectl apply -f ../../Services/Nginx-Service.yaml
 sleep 5
 echo "Traefik"
 kubectl apply -f ../Ingress/Traefik-Ingress.yaml
