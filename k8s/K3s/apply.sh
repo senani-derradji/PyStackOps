c030bdfcc6f3348cb876b6cbe80b3fb9cbf9f8f2
senani-derradji <derradji.senani.1@gmail.com>
2025-08-21 02:17:28 +0100

    K3S -> PRODUCTION LEVEL

---
+++ k8s/K3s/apply.sh
@@ -0,0 +1,16 @@
+kubectl apply -f ../Secrets/
+kubectl apply -f ../ConfigMaps/
+kubectl apply -f ../PVCs/
+
+echo "MYSQL"
+kubectl apply -f ../Deployments/Mysql-Deployment.yaml
+kubectl apply -f ../Services/Mysql-Service.yaml
+
+sleep 20
+echo "REDIS"
+kubectl apply -f ../Deployments/Redis-Deployment.yaml
+kubectl apply -f ../Services/Redis-Service.yaml
+sleep 5
+echo "Flask"
+kubectl apply -f ..//Deployments/Flask-Deployment.yaml
+kubectl apply -f ../Services/Flask-Service.yaml
