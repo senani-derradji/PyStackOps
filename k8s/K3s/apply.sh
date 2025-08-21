ed95f5f2c6f575f0e6a5b6d957bbbc6001b37921
senani-derradji <derradji.senani.1@gmail.com>
2025-08-21 03:12:52 +0100

    K3S - Prod - Done

--- k8s/K3s/apply.sh
+++ k8s/K3s/apply.sh
@@ -1,6 +1,6 @@
 kubectl apply -f ../Secrets/
 kubectl apply -f ../ConfigMaps/
-kubectl apply -f ../PVCs/
+kubectl apply -f PVCs/
 
 echo "MYSQL"
 kubectl apply -f ../Deployments/Mysql-Deployment.yaml
@@ -14,3 +14,10 @@ sleep 5
 echo "Flask"
 kubectl apply -f ..//Deployments/Flask-Deployment.yaml
 kubectl apply -f ../Services/Flask-Service.yaml
+sleep 5
+echo "Nginx"
+kubectl apply -f ../Deployments/Nginx-Deployment.yaml
+kubectl apply -f ../Services/Nginx-Service.yaml
+sleep 5
+echo "Traefik"
+kubectl apply -f ../Ingress/Traefik-Ingress.yaml
