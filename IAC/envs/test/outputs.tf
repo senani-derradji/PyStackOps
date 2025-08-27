39360fab30e0ff1a13cf853ab716a16d9e275b99
senani-derradji <derradji.senani.1@gmail.com>
2025-08-27 11:12:27 +0100

    TEST ENV START

---
+++ IAC/envs/test/outputs.tf
@@ -0,0 +1,29 @@
+output "test_vm_ip" {
+  value       = module.compute.public_ip_address
+  sensitive   = true
+}
+
+output "test_vm_name" {
+  value       = module.compute.vm_name
+}
+
+output "test_mysql_fqdn" {
+  value       = module.database.mysql_fqdn
+  sensitive   = true
+}
+
+output "test_mysql_admin" {
+  value       = module.database.mysql_admin
+  sensitive   = true
+}
+
+output "test_mysql_password" {
+  value       = module.database.mysql_password
+  sensitive   = true
+}
+
+output "test_mysql_db_name" {
+  value       = module.database.mysql_db_name
+}
+
+
