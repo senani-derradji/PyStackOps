fb489a10ee2d5e73c2250c44a9cc2d492b3d293b
senani-derradji <derradji.senani.1@gmail.com>
2025-08-27 17:10:51 +0100

    TEST ENV START

--- IAC/modules/compute/main.tf
+++ IAC/modules/compute/main.tf
@@ -24,6 +24,30 @@ resource "azurerm_network_interface" "network_interface" {
 #   network_security_group_id     = var.nsg_id
 # }
 
+resource "azurerm_network_security_group" "vm_nsg" {
+  name                = "vm-nsg"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "nic_nsg" {
+  network_interface_id      = azurerm_network_interface.network_interface.id
+  network_security_group_id = azurerm_network_security_group.vm_nsg.id
+}
+
+
 
 resource "azurerm_linux_virtual_machine" "vm" {
   name                  = var.vm_name
